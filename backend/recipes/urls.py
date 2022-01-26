26b78fbc478c30e4134cd6a08c253b7d17818036
ViktoriaPashkovskaja <pashkovskajava@yandex.ru>
2022-01-26 17:57:02 +0300

    Проверка

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import IngredientsViewSet, RecipeViewSet, TagViewSet
 
 router = DefaultRouter()
-router.register('tags', TagViewSet, 'Tag')
-router.register('recipes', RecipeViewSet, 'recipe')
+router.register('tags', TagViewSet, 'tags')
+router.register('recipes', RecipeViewSet, 'recipes')
 router.register('ingredients', IngredientsViewSet, 'ingredients')
 urlpatterns = [
     path('', include(router.urls)),
