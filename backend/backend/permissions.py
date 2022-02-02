95edb7d814df5324d203027aabdd910d5cb7582c
ViktoriaPashkovskaja <pashkovskajava@yandex.ru>
2022-02-02 14:26:27 +0300

    Проверка

--- backend/backend/permissions.py
+++ backend/backend/permissions.py
@@ -1,15 +1,7 @@
-from rest_framework import permissions
+from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
-class AuthorOrReadOnly(permissions.BasePermission):
-    def has_permission(self, request, view):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or request.user.is_authenticated
-        )
+class IsAuthorOrAdminOrReadOnly(BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        return (
-            request.method in permissions.SAFE_METHODS
-            or obj.author == request.user
-        )
+        return request.method in SAFE_METHODS or obj.author == request.user
